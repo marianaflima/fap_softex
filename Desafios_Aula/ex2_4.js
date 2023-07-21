5d1e8fb9ade32be10bd25cd26aef5f65a961fd58
Mari Félix <marianafelixdelima7@gmail.com>
2023-07-21 00:21:12 -0300

    Update codepark

---
+++ Desafios_Aula/ex2_4.js
@@ -0,0 +1,21 @@
+let diaSemana = Number(prompt('Digite um número para dia da semana: '))
+
+if (diaSemana >= 1 < 8) {
+    if (diaSemana === 1) {
+        console.log('Domingo')
+    } else if (diaSemana === 2) {
+        console.log('Segunda')
+    } else if (diaSemana === 3 ){
+        console.log('Terça')
+    } else if (diaSemana === 4) {
+        console.log('Quarta')
+    } else if (diaSemana === 5) {
+        console.log('Quinta')
+    } else if (diaSemana === 6) {
+        console.log('Sexta')
+    } else if (diaSemana === 7) {
+        console.log('Sábado')
+    } else {
+    console.log('Valor inválido!');
+}
+}
