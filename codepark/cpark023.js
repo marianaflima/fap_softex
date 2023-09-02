05e433744b6738651b351cab842c25d5dd7f775d
Mariana <marianafelixdelima7@gmail.com>
2023-09-02 19:31:16 -0300

    adding cpark023.js

---
+++ codepark/cpark023.js
@@ -0,0 +1,14 @@
+const prompt = require('prompt-sync')();
+
+let nome, salario, idade, temDiploma;
+
+/*
+Nome: string
+salario: number
+idade: number
+temDiploma: boolean
+*/
+nome = 'João da Siva';
+salario = 5000.00;
+idade = 30;
+temDiploma = true;
