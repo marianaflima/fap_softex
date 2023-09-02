96e1b06b7a2dbdfc78d5ce9bcc1ab40c6ba49419
Mariana <marianafelixdelima7@gmail.com>
2023-09-01 21:51:21 -0300

    add cparks 020 & 021

--- codepark/cpark020.js
+++ codepark/cpark020.js
@@ -1,134 +1,48 @@
 class Node {
     constructor(value) {
-        this.value = value;
-        this.next = null;
+      this.value = value;
+      this.next = null;
     }
-
-    set next(value) {
-        this.next = value;
+  }
+  
+  class LinkedList {
+    constructor() {
+      this.head = null;
     }
-
-    get next() {
-        return this.next;
-    }
-
-    get value() {
-        return this.value;
-    }
-}
-
-class LinkedList {
-    constructor(value) {
-        this.head = null;
-        this.size = 0;
-
-        if (value !== undefined) {
-            this.append(value);
-            if (Array.isArray(value)) {
-                for(let element of value) {
-                    this.append(element);
-                }
-            } else {
-                this.append(value);
-            }
-        }
-    }
-
-    get size() {
-        return this.size;
-    }
-
-    get isEmpty() {
-        return this.size === 0;
-    }
-    
-    get head() {
-        return this.head;
-    }
-
-    set head(value) {
-        this.head = value;
-    }
-
-    _getByIndex(index) {
-        let previousNode = null;
-        let currentNode = this.head;
-        while (index > 0) {
-            previousNode = currentNode;
-            currentNode = currentNode.next;
-            index--;
-        }
-
-        return [currentNode, previousNode];
-    }
-
-    get(index) {
-        let [currentNode,_] = this._getByIndex(index);
-        return currentNode.value;
-    }
-
-    insert(node, value) {
-        let newNode = new LinkedList(value);
-
-        if (node == null) {
-            newNode.next = this.head;
-            this.head = newNode;
-        } else {
-            let next = node.next;
-            node.next = newNode;
-            node.next.next = next;
-        }
-
-        this.size = this.size + 1;
-        return node;
-    }
-
-    insertAt(value, index) {
-        let [_, previous] = this._getByIndex(index);
-        this.insert(previous, value);
-    }
-
-    add(value) {
-        this.insert(null, value);
-    }
-
+  
+    // Adicionar um nó no final da lista
     append(value) {
-        this.insertAt(value, this.size);
+      const newNode = new Node(value);
+  
+      if (!this.head) {
+        this.head = newNode;
+        return;
+      }
+  
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+  
+      current.next = newNode;
     }
-
-    remove(index) {
-        let [currentNode, previousNode] = this._getByIndex(index);
-
-        if (previousNode !== null) {
-            previousNode.next = currentNode.next;
-        } else {
-            this.head = currentNode.next;
-        }
-
-        this.size = this.size - 1;
+  
+    // Exibir os valores da lista
+    display() {
+      let current = this.head;
+      while (current) {
+        console.log(current.value);
+        current = current.next;
+      }
     }
-
-    *values() {
-        let current = this.head;
-        while (current !== null) {
-            yield current.value;
-            current = current.next;
-        }
-    }
-
-    [Symbol.iterator]() {
-        return this.values();
-    }
-
-    toArray() {
-        let array = [];
-        for (let node of this)
-            array.push(node);
-
-        return array;
-    }
-}
-
-let listaEncadeada = new LinkedList(['Albert', 'Elizabeth', 'Charles']);
-
-console.log(listaEncadeada)
+  }
+  
+  // Criando uma lista encadeada e adicionando elementos
+  const myList = new LinkedList();
+  myList.append('Albert');
+  myList.append('Elizabeth');
+  myList.append('Charles');
+  
+  // Exibindo os valores da lista
+  myList.display();
+  
