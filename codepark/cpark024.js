5facc907e34c06565e50bf1f69839d228f061eed
Mariana <marianafelixdelima7@gmail.com>
2023-09-02 20:36:45 -0300

    adding cpark024

---
+++ codepark/cpark024.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')();
+
+let nota1, nota2, nota3, media;
+
+nota1 = Number(prompt("Digite a 1ª nota: "));
+nota2 = Number(prompt("Digite a 2ª nota: "));
+nota3 = Number(prompt("Digite a 3ª nota: "));
+
+media = (nota1 + nota2 + nota3) / 3;
+
+console.log(`Sua nota final é: ${media.toFixed(2)}`);
